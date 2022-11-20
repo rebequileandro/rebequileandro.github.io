74bf848b55f3f2c9af2fbb260a5885e0d7463a3c
leandro-rebequi <rebequileandro@gmail.com>
2022-11-20 05:50:47 -0300

    -

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import './styles/AboutMe.scss';
 import { useObserver } from '@/hooks';
+import lr1 from '@/assets/leandro-rebequi1.webp';
+import lr2 from '@/assets/leandro-rebequi2.webp';
 
 export interface AboutMeInterface {
   serInView: React.Dispatch<React.SetStateAction<string>>;
@@ -17,7 +19,41 @@ const AboutMe: React.FC<AboutMeInterface> = ({ serInView }) => {
 
   return (
     <section className="aboutme" ref={setReference} id="about-me">
-      AboutMe
+      <div
+        className={`aboutme__photos aboutme__photos--${
+          isIntersecting ? 'in' : 'out'
+        }`}
+      >
+        <img src={lr1} alt="leandro rebequi" className="aboutme__photos__img" />
+      </div>
+      <div className="aboutme__decription-container">
+        <h2
+          className={`text-primary--sub aboutme__sub-title aboutme__sub-title--${
+            isIntersecting ? 'in' : 'out'
+          }`}
+        >
+          Sobre mi
+        </h2>
+        <p
+          style={{}}
+          className={`text-secondary--sub aboutme__description aboutme__description--${
+            isIntersecting ? 'in' : 'out'
+          }`}
+        >
+          ¡Hola! Mi nombre es Leandro Rebequi y me apasioná crear cosas desde 0
+          y ver como se trasforman en algo increíble. <br />
+          <br />
+          Mis inicios en el desarrollo web comenzaron a mediados del 2019,
+          comencé haciendo simples maquetaciones en HTML y CSS, inspeccionando
+          códigos de otras webs y viendo cursos en YouTube.
+          <br />
+          Luego por el 2021, comencé a capacitarme más en serio, tome un
+          Bootcamp Full Stack, donde adquirí una buena base para crear webs y
+          aplicaciones completas, a partir de ese punto comencé a capacitarme
+          por mi cuenta a medida que los trabajos o proyectos lo iban
+          requiriendo.
+        </p>
+      </div>
     </section>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,53 +6,70 @@ import { useObserver } from '@/hooks';
 
 export interface HomeInterface {
   serInView: React.Dispatch<React.SetStateAction<string>>;
+  loadFirstAnimation: boolean;
+  scroll: number;
 }
 const Home: React.FC<HomeInterface> = ({ serInView }) => {
   const [loadFirstAnimation, setLoadFirstAnimation] = useState(false);
+  const [scroll, setScroll] = useState(window.scrollY);
   const [isIntersecting, setReference] = useObserver({
     root: null,
     threshold: 0.5
   });
+
   useEffect(() => {
     isIntersecting && serInView('home');
   }, [isIntersecting]);
-
-  const refAn = useRef(null);
-  const inViewAn = false;
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadFirstAnimation(true);
+    }, 13000);
+    window.addEventListener('scroll', () => {
+      window.scrollY < 210 && setScroll(window.scrollY);
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {
+        window.scrollY < 210 && setScroll(window.scrollY);
+      });
+    };
+  }, []);
   return (
     <section ref={setReference} className="home" id="home">
-      <div ref={refAn}>
+      <div>
         <h1
           className={`home__seg-1 home__seg-1--${
-            !loadFirstAnimation ? 'first' : inViewAn ? 'in' : 'out'
+            !loadFirstAnimation ? 'first' : isIntersecting ? 'in' : 'out'
           } text-primary--main`}
         >
           Full Stack Developer <br />
         </h1>
         <h1
           className={`home__seg-2 home__seg-2--${
-            !loadFirstAnimation ? 'first' : inViewAn ? 'in' : 'out'
+            !loadFirstAnimation ? 'first' : isIntersecting ? 'in' : 'out'
           } text-primary--main`}
         >
           Creo experiencias, <br />
         </h1>
         <h1
           className={`home__seg-2 home__seg-2--${
-            !loadFirstAnimation ? 'first' : inViewAn ? 'in' : 'out'
+            !loadFirstAnimation ? 'first' : isIntersecting ? 'in' : 'out'
           } text-primary--main`}
         >
           Desarrollo tus ideas
         </h1>
         <p
-          className={`home__seg-3 home__seg-3--${
-            !loadFirstAnimation ? 'first' : inViewAn ? 'in' : 'out'
+          className={`home__seg-3--${
+            !loadFirstAnimation ? 'first' : isIntersecting ? 'in' : 'out'
           } text-secondary--main`}
         >
           Mi nombre es Leandro Rebequi, soy <br />
           desarrollador multiplataforma.
         </p>
       </div>
-      <div className="home__animation-container">
+      <div
+        className="home__animation-container"
+        style={{ transform: `translateY(${scroll / 9}%)` }}
+      >
         <span className="home__animation-container__glow" />
         <Lottie
           options={{
