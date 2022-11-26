188217e099c59af8b8d0577b95aaaa02a4d31f09
leandro-rebequi <rebequileandro@gmail.com>
2022-11-25 21:24:21 -0300

    -

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
