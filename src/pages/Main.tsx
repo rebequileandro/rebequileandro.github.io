ef33515a7e44589c381231debd94d93f6a726f75
leandro-rebequi <rebequileandro@gmail.com>
2024-04-18 15:30:02 -0300

    a

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -9,7 +9,7 @@ export interface MainInterface {}
 
 const Main: React.FC<MainInterface> = () => {
   const [inView, serInView] = useState<string>('home');
-
+  console.log('<A></A>');
   return (
     <>
       <div className="main__line0">
